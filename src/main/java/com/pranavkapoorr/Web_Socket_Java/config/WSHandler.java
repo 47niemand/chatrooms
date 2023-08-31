a133483627026035326c3accbd1898d479f5fb43
Dmytro Muza <5021630+47niemand@users.noreply.github.com>
2023-08-31 21:18:41 +0300

    update

--- src/main/java/com/pranavkapoorr/Web_Socket_Java/config/WSHandler.java
+++ src/main/java/com/pranavkapoorr/Web_Socket_Java/config/WSHandler.java
@@ -5,6 +5,10 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.google.gson.Gson;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
@@ -12,53 +16,50 @@ import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 
-import com.google.gson.Gson;
-
 @Component
-public class WSHandler implements WebSocketHandler{
-	Map<WebSocketSession,String> connectionsMap = new HashMap<>();
+public class WSHandler implements WebSocketHandler {
+
+	Logger logger = LoggerFactory.getLogger(WSHandler.class);
+	Map<WebSocketSession, String> connectionsMap = new HashMap<>();
 
 	@Override
 	public void afterConnectionClosed(WebSocketSession sess, CloseStatus arg1) throws Exception {
 		connectionsMap.remove(sess);
-		System.out.println("connection removed : active connections: "+connectionsMap.size());
-		
+		logger.info("connection removed : active connections: " + connectionsMap.size());
 	}
 
 	@Override
 	public void afterConnectionEstablished(WebSocketSession sess) throws Exception {
-		connectionsMap.put(sess,"");
-		System.out.println("connection added : active connections: "+connectionsMap.size());
-		
+		connectionsMap.put(sess, "");
+		logger.info("connection added : active connections: " + connectionsMap.size());
+
 	}
 
 	@Override
-	public void handleMessage(WebSocketSession sess, WebSocketMessage<?> message) throws Exception {
-		System.out.println("received payload : "+message.getPayload().toString());
-		Map value = new Gson().fromJson(message.getPayload().toString(), Map.class);
-		if(value.get("name")!=null){
+	public void handleMessage(WebSocketSession sess, WebSocketMessage<?> message) {
+		logger.info("received payload : " + message.getPayload().toString());
+		var value = new Gson().fromJson(message.getPayload().toString(), Map.class);
+		if (value.get("name") != null) {
 			connectionsMap.replace(sess, "", value.get("name").toString());
-		}else{
-			Arrays.asList(connectionsMap.keySet().toArray()).forEach((s)->{
+		} else {
+			Arrays.asList(connectionsMap.keySet().toArray()).forEach((s) -> {
 				try {
-					System.out.println("sending out by "+ connectionsMap.get(sess) +" to "+ connectionsMap.get(s));
-					((WebSocketSession) s).sendMessage(new TextMessage(connectionsMap.get(sess) +" : "+ value.get("message")));
+					System.out.println("sending out by " + connectionsMap.get(sess) + " to " + connectionsMap.get(s));
+					((WebSocketSession) s)
+							.sendMessage(new TextMessage(connectionsMap.get(sess) + " : " + value.get("message")));
 				} catch (IOException e) {
 					System.err.println(e.getMessage());
 				}
 			});
 		}
-		
-	
-		
-		
-		
+
 	}
 
 	@Override
 	public void handleTransportError(WebSocketSession sess, Throwable arg1) throws Exception {
+		logger.error("error occured at sender " + sess, arg1);
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
--- src/main/java/com/pranavkapoorr/Web_Socket_Java/config/WebSocketConfig.java
+++ src/main/java/com/pranavkapoorr/Web_Socket_Java/config/WebSocketConfig.java
@@ -5,14 +5,19 @@ import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
 
+	Logger logger = LoggerFactory.getLogger(WebSocketConfig.class);
+
 	@Override
 	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		logger.info("registering handler");
 		registry.addHandler(new WSHandler(), "/name").setAllowedOrigins("*");
 	}
-	
 
 }
